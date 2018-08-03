cd99183506dd6f3f2ecf7a9ebb310828f0e9d97c
Marissa <marissa.biesecker@gmail.com>
2018-08-03 11:10:49 +0100

    Click Count dynamic for each picture

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 let heroImg = document.getElementsByClassName('heroImg');
 let countTitle = document.getElementsByClassName('countTitle');
 let count = document.getElementsByClassName('count');
-let counterRay = 0;
-let counterLita= 0;
+// let counterRay = 0;
+// let counterLita = 0;
+//
+//
+// heroImg[0].addEventListener('click', function(){
+//   console.log('left-click');
+//   counterRay ++
+//   count[0].innerHTML = counterRay;
+// });
+// heroImg[1].addEventListener('click', function(){
+//   console.log('right-click');
+//   counterLita ++
+//   count[1].innerHTML = counterLita;
+// });
+
+for (var i = 0; i < heroImg.length; i++){
+  let cat = heroImg[i];
+  let counter = 0;
+  let loveCounter = count[i];
+  count[i].textContent = counter;
+
+  cat.addEventListener('click', function(countCopy){
+    return function(){
+      counter ++
+      loveCounter.textContent = counter;
+    };
 
 
-heroImg[0].addEventListener('click', function(){
-  console.log('left-click');
-  counterRay ++
-  count[0].innerHTML = counterRay;
-});
-heroImg[1].addEventListener('click', function(){
-  console.log('right-click');
-  counterLita ++
-  count[1].innerHTML = counterLita;
-});
 
+  }(cat));
+}
 
+//I think I should be making these cats with a constructor
 // class Cat {
 //   constructor (name, love) {
 //     this.name = name;
